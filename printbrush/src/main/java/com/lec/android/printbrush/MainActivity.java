2a70cb4de5644ac82e41146a1d0a59e5b85f12ff
KIMJINYOUNG9408 <kalkungo9408@gmail.com>
2020-05-06 11:44:33 +0900

    2020-05-06 a(2)

--- printbrush/src/main/java/com/lec/android/printbrush/MainActivity.java
+++ printbrush/src/main/java/com/lec/android/printbrush/MainActivity.java
@@ -1,7 +1,7 @@
 package com.lec.android.printbrush;
 // 버튼 8개 만들어서 각각 기능을 하고 밑에 뷰에 그림을 그리도록 한다.
 // 그 그림을 저장하는 기능 ?.. 또 사진을 불러와서 그림을 수정할수잇는 기능 ?.
-// 편집 그림판 ? 
+// 편집 그림판 ? f 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
